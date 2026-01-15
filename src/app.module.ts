02fc44af882c88bed5d61eae5d4cca64dbe96f9c
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-01-15 07:47:36 +0000

    feat: core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { ReportingsModule } from './reportings/reportings.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule, PaymentsModule, ProfilesModule, ReportingsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
