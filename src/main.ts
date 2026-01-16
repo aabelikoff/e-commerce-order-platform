6b843ed8aad3f400ab8078c02f6657f4ad79e6e2
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-01-16 18:50:42 +0000

    upd(main): change name for config

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { ConfigType } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+  const configService = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
 
-  await app.listen(config.port || 3001);
+  await app.listen(configService.port || 3001);
 }
 bootstrap();
