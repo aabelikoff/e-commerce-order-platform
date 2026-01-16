4a7400b967c5ea28abadcecef2212437935ee839
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-01-16 09:18:05 +0000

    chore: impl ConfigModul in main for port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './config/app/app.config';
+import { ConfigType } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+
+  await app.listen(config.port || 3001);
 }
 bootstrap();
