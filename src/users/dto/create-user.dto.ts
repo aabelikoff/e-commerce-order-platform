1d1c400e06ac8daad1623e9b8fe21d2261948a94
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-01-18 19:52:59 +0000

    upd(create-user-dto): add class validator decor

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,21 @@
-
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
 }
