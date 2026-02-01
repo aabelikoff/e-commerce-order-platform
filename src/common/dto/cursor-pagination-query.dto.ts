2bc9a9f666ab3234013453d5f70fb56aab597f4c
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-01 22:06:39 +0000

    feat(cursor-pagination-query-dto): add

--- src/common/dto/cursor-pagination-query.dto.ts
+++ src/common/dto/cursor-pagination-query.dto.ts
@@ -1,12 +1,16 @@
 import { IsInt, Min, IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CursorPaginationQueryDto {
+  @ApiPropertyOptional({ example: 10 })
+  @IsOptional()
   @IsInt()
   @Min(1)
-  @Transform(({ value }) => value ? Number(value) : 10)
-  limit: number;
+  @Transform(({ value }) => (value ? Number(value) : 10))
+  limit: number = 10;
 
+  @ApiPropertyOptional({ example: 'eyJpZCI6ICJzb20gdXVpZCIsICJjcmVhdGVkQXQiOiAic29tIGRhdGUifQ==' })
   @IsOptional()
   @IsString()
   cursor?: string;
