0a5224336c4bfe3928f17cfd81f70dc69ff03142
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-04 19:25:40 +0000

    feat: basic seeds for demo

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,19 +1,36 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
-import { resolve } from "path";
-import { getEnvFilePath } from "../config/env/env-path.config";
-
+import { join, resolve } from 'path';
+import { getEnvFilePath } from '../config/env/env-path.config';
+import { SeederOptions } from 'typeorm-extension';
 
 config({ path: resolve(process.cwd(), getEnvFilePath()) });
 
-export const AppDataSource= new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: ['src/**/*.entity.ts'],
-    migrations: ['src/database/migrations/*.ts'],
-    synchronize: false
-})
+const seedsGlob = join(__dirname, 'seeds', '**', '*.seeder{.ts,.js}').replace(
+  /\\/g,
+  '/',
+);
+const migrationsGlob = join(__dirname, 'migrations', '*{.ts,.js}').replace(
+  /\\/g,
+  '/',
+);
+const entitiesGlob = join(__dirname, '..', '**', '*.entity{.ts,.js}').replace(
+  /\\/g,
+  '/',
+);
+
+const options: DataSourceOptions & SeederOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [entitiesGlob],
+  migrations: [migrationsGlob],
+  seeds: [seedsGlob],
+  synchronize: false,
+  seedTracking: true,
+};
+
+export const AppDataSource = new DataSource(options);
--- src/database/utills/assert-seed-allowed.util.ts
+++
@@ -1,8 +0,0 @@
-export function assertSeedAllowed() {
-  const env = process.env.NODE_ENV;
-  const enabled = process.env.SEED_ENABLED === "true";
-
-  if (env !== "development" || !enabled) {
-    throw new Error("❌ Seed is not allowed in this environment");
-  }
-}
