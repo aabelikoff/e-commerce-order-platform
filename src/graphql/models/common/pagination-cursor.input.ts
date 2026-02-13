d70f2b0c2fc938e2ab376e528af6a6194432945e
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-13 22:29:07 +0000

    chore(pagination-cursor-input): add max validation for max value

--- src/graphql/models/common/pagination-cursor.input.ts
+++ src/graphql/models/common/pagination-cursor.input.ts
@@ -1,14 +1,11 @@
-import {
-  Field,
-  InputType,
-  Int,
-} from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 import {
   ValidateNested,
   IsOptional,
   IsInt,
   Min,
   IsString,
+  Max,
 } from 'class-validator';
 
 @InputType()
@@ -16,6 +13,7 @@ export class PaginationCursorInput {
   @Field(() => Int)
   @IsInt()
   @Min(1)
+  @Max(100)
   limit: number = 20;
 
   @Field(() => String, { nullable: true })
