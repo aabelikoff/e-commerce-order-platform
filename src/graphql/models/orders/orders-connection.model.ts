5ae5d5aac5e1941a3652616257c50541b07c97f0
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-14 18:37:06 +0000

    feat(create-paginated): add

--- src/graphql/models/orders/orders-connection.model.ts
+++ src/graphql/models/orders/orders-connection.model.ts
@@ -1,25 +1,31 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { OrderModel } from './order.model';
 import { PageInfo } from '../common/page-info.model';
+import { CreatePaginatedType } from '../../types/creat-paginated.type';
+
+// @ObjectType({
+//   description:
+//     'Paginated list of orders following the Connection pattern',
+// })
+// export class OrdersConnection {
+//   @Field(() => [OrderModel], {
+//     nullable: false,
+//     description: 'List of orders matching the provided filter and pagination',
+//   })
+//   nodes: OrderModel[];
+
+//   @Field(() => PageInfo, {
+//     description: 'Pagination metadata (cursor and navigation state)',
+//   })
+//   pageInfo: PageInfo;
+
+//   @Field(() => Int, {
+//     description: 'Total number of orders matching the filter (ignores pagination limit)',
+//   })
+//   totalCount: number;
+// }
 
 @ObjectType({
-  description:
-    'Paginated list of orders following the Connection pattern',
+  description: 'Paginated list of orders following the Connection pattern',
 })
-export class OrdersConnection {
-  @Field(() => [OrderModel], {
-    nullable: false,
-    description: 'List of orders matching the provided filter and pagination',
-  })
-  nodes: OrderModel[];
-
-  @Field(() => PageInfo, {
-    description: 'Pagination metadata (cursor and navigation state)',
-  })
-  pageInfo: PageInfo;
-
-  @Field(() => Int, {
-    description: 'Total number of orders matching the filter (ignores pagination limit)',
-  })
-  totalCount: number;
-}
+export class OrdersConnection extends CreatePaginatedType(OrderModel, false) {}
