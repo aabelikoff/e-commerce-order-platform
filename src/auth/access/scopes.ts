9ca46f789f7edbf123816cca61c7cb384c7ee49b
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-19 19:48:41 +0000

    feat: scopes and roles enums

---
+++ src/auth/access/scopes.ts
@@ -0,0 +1,23 @@
+export enum OrderScopes {
+  ORDER_READ = 'order:read',
+  ORDER_WRITE = 'order:write',
+}
+
+export enum ProductScopes {
+  PRODUCT_READ = 'product:read',
+  PRODUCT_WRITE = 'product:write',
+}
+
+export enum UserScopes {
+  USER_READ = 'user:read',
+  USER_WRITE = 'user:write',
+}
+
+export enum PaymentScopes {
+  PAYMENT_READ = 'payment:read',
+  PAYMENT_WRITE = 'payment:write',
+}
+
+export enum RefundScopes {
+  REFUND_WRITE = 'refund:write',
+}
