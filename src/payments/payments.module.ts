b5adbf09d8af4ea72412b2d50aa880915ee8d321
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-20 14:37:30 +0000

    chore(payments-module): fix imports

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,8 +3,13 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { CanPayGuard } from './guards/can-pay.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Type } from 'class-transformer';
+import { Payment } from 'src/database/entities/payment.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'src/database/entities/order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   controllers: [PaymentsController],
   providers: [PaymentsService, CanPayGuard, JwtAuthGuard],
 })
