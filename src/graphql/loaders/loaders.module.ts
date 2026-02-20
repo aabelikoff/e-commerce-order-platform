cefe1b8b5765cb77ace3b853a510715552282b48
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-02-20 12:43:51 +0000

    chore(loaders-module): add paymentsByOrderIdLoader

--- src/graphql/loaders/loaders.module.ts
+++ src/graphql/loaders/loaders.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User, Product, OrderItem } from '../../database/entities';
+import { User, Product, OrderItem, Payment } from '../../database/entities';
 import { LoadersFactory } from './loaders.factory';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Product, OrderItem])],
+  imports: [TypeOrmModule.forFeature([User, Product, OrderItem, Payment])],
   providers: [LoadersFactory],
   exports: [LoadersFactory]
 })
--- src/graphql/loaders/loaders.types.ts
+++ src/graphql/loaders/loaders.types.ts
@@ -1,11 +1,12 @@
 import DataLoader from 'dataloader';
-import { User, Product, Order, OrderItem } from '../../database/entities';
+import { User, Product, Order, OrderItem, Payment } from '../../database/entities';
 
 
 export type AppLoaders = {
   userByIdLoader: DataLoader<string, User | null>;
   productByIdLoader: DataLoader<string, Product | null>;
   orderItemsByOrderIdLoader: DataLoader<string, OrderItem[]>;
+  paymentsByOrderIdLoader: DataLoader<string, Payment[]>;
 };
 
 export type GraphQLContext = {
