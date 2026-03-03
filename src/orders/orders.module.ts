8e7317e1e40954135d330c19ea3b01b58c33660f
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-03-03 14:38:55 +0000

    chore(orders-module): update iimport with RabbitmqModule and providers with OrdersProcessorConsumer

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,11 +4,13 @@ import { OrdersService } from './orders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderItem, Order, Product, User } from 'src/database/entities';
 import { OrdersEventsService } from './orders-events.service';
+import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
+import { OrdersProcessorConsumer } from './orders-processor.consumer';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product, User])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product, User]), RabbitmqModule],
   controllers: [OrdersV1Controller],
-  providers: [OrdersService, OrdersEventsService],
+  providers: [OrdersService, OrdersEventsService, OrdersProcessorConsumer],
   exports: [OrdersEventsService, OrdersService]
 })
 export class OrdersModule {}
