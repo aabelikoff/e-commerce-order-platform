4647dd301cb7138889561ac92a92b5940efabae2
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-03-03 14:34:36 +0000

    feat(rabbitmq-module): create with service and topology

---
+++ src/rabbitmq/rabbitmq.topology.ts
@@ -0,0 +1,5 @@
+export const ORDERS_EXCHANGE = 'orders.exchange';
+export const ORDERS_PROCESS_QUEUE = 'orders.process';
+export const ORDERS_DLQ_QUEUE = 'orders.dlq';
+export const ORDERS_PROCESS_ROUTING_KEY = 'orders.process';
+export const ORDERS_DLQ_ROUTING_KEY = 'orders.dlq';
