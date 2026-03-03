1314403bc04f797dafe38621904a8d50406a1e47
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-03-03 14:35:09 +0000

    chore(rabbitmq-config): create with types

--- src/config/rabbitmq/rabbitmq.config.ts
+++ src/config/rabbitmq/rabbitmq.config.ts
@@ -6,6 +6,8 @@ export const rabbitMQConfig = registerAs(
   (): IRabbitMq => ({
     url: process.env.RABBITMQ_URL ?? '',
     prefetch: Number(process.env.RABBITMQ_PREFETCH ?? '1'),
+    maxAttempts: Number(process.env.RABBITMQ_MAX_ATTEMPTS ?? '3'),
+    retryDelayMs: Number(process.env.RABBITMQ_RETRY_DELAY_MS ?? '5000'),
     outbox_relay_interval: Number(
       process.env.OUTBOX_RELAY_INTERVAL_MS ?? '1000',
     ),
--- src/config/rabbitmq/rabbitmq.types.ts
+++ src/config/rabbitmq/rabbitmq.types.ts
@@ -1,6 +1,9 @@
 export interface IRabbitMq {
   url: string;
   prefetch: number;
+  maxAttempts: number;
+  retryDelayMs: number;
+
   outbox_relay_interval: number;
   outbox_relay_batch_size: number;
 }
