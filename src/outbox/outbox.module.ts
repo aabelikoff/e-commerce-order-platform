3c0d91a6e5ab904d3ad6808f0efff51332061dbc
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-03-04 10:46:54 +0000

    chore(outbox-module): add RabbitmqModule to imports

--- src/outbox/outbox.module.ts
+++ src/outbox/outbox.module.ts
@@ -3,9 +3,10 @@ import { OutboxService } from './outbox.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OutboxEvent } from 'src/database/entities';
 import { OutboxRelayService } from './outbox-relay.service';
+import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OutboxEvent])],
+  imports: [TypeOrmModule.forFeature([OutboxEvent]), RabbitmqModule],
   providers: [OutboxService,OutboxRelayService],
   exports: [OutboxService, OutboxRelayService],
 })
