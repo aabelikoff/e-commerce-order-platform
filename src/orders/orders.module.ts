f040ce4bcc6c5923ce4dfec6403244adc34e8200
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-03-07 21:07:20 +0000

    chore(orders-module): update imports & providers with consumers

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -7,11 +7,25 @@ import { OrdersEventsService } from './orders-events.service';
 import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
 import { OrdersProcessorConsumer } from './orders-processor.consumer';
 import { OutboxModule } from 'src/outbox/outbox.module';
+import { KafkaModule } from 'src/kafka/kafka.module';
+import { OrdersAnalyticsConsumer } from './orders-analytics.consumer';
+import { OrdersCrmConsumer } from './orders-crm.consumer';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product, User]), RabbitmqModule, OutboxModule],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Product, User]),
+    RabbitmqModule,
+    OutboxModule,
+    KafkaModule,
+  ],
   controllers: [OrdersV1Controller],
-  providers: [OrdersService, OrdersEventsService, OrdersProcessorConsumer],
-  exports: [OrdersEventsService, OrdersService]
+  providers: [
+    OrdersService,
+    OrdersEventsService,
+    OrdersProcessorConsumer,
+    OrdersAnalyticsConsumer,
+    OrdersCrmConsumer,
+  ],
+  exports: [OrdersEventsService, OrdersService],
 })
 export class OrdersModule {}
