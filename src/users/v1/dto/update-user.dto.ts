ec7ac68febc2730498e64b0e2b5cf2840a75306a
Oleksii Bielikoff <aabelikoff@gmail.com>
2026-04-29 23:08:21 +0100

    Fix auth profile e2e coverage

--- src/users/v1/dto/update-user.dto.ts
+++ src/users/v1/dto/update-user.dto.ts
@@ -1,20 +1,29 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsEmail,
+  IsOptional,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @ApiPropertyOptional({ description: 'First name', type: 'string' })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @MinLength(2)
   firstName?: string;
 
   @ApiPropertyOptional({ description: 'Last name', type: 'string' })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @MinLength(2)
   lastName?: string;
 
   @ApiPropertyOptional({ description: 'Email', type: 'string' })
+  @IsOptional()
   @IsEmail()
   email?: string;
 }
